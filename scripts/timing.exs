6d1113ea10c9a357223cb4acdad4e6c53b909815
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-01 08:26:50 +0200

    some more testing timing out

--- scripts/timing.exs
+++ scripts/timing.exs
@@ -3,6 +3,15 @@ start = :os.system_time :nano_seconds
 finish = :os.system_time :nano_seconds
 IO.puts "nanos measuring difference: #{finish - start}"
 
+fun2 = fn -> Enum.map(Enum.to_list(1..100), fn(i) -> i * i end) end
+
+{time, _} = :timer.tc fun2
+IO.puts "first fun2 timer time: #{time}"
+
+{time, _} = :timer.tc fun2
+IO.puts "second fun2 timer time: #{time}"
+
+
 {time, _return} = :timer.tc fn -> 0 end
 IO.puts "Empty timer.tc #{time}"
 
