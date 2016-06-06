2d0add6c81c75a647e59fde31d83e783fdbd6d43
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-06 23:14:11 +0200

    use benchee

--- bench/my_map_bench.exs
+++ bench/my_map_bench.exs
@@ -1,18 +1,7 @@
-defmodule MyMapBench do
-  use Benchfella
+list = Enum.to_list(1..10_000)
 
-  @list Enum.to_list(1..10_000)
-
-  bench "map with TCO reverse" do
-    MyMap.map(@list, fn(i) -> i + 1 end)
-  end
-
-  bench "map with TCO and ++" do
-    MyMap.map2(@list, fn(i) -> i + 1 end)
-  end
-
-  bench "map simple without TCO" do
-    MyMap.map3(@list, fn(i) -> i + 1 end)
-  end
-
-end
+Benchee.run [
+  {"map with TCO reverse", fn -> MyMap.map(list, fn(i) -> i + 1 end) end},
+  {"map with TCO and ++", fn -> MyMap.map2(list, fn(i) -> i + 1 end) end},
+  {"map simple without TCO", fn -> MyMap.map3(list, fn(i) -> i + 1 end) end}
+]
