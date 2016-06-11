7f43d5a37d521fa772439dbaac1c11c3c53333c9
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-11 09:56:11 +0200

    Updated map benchmark with new variants
    * noticed when moving body recursive around that there seems
      to be some sort of warmup going on as it performed worse
      when it was the first one

--- bench/my_map_bench.exs
+++ bench/my_map_bench.exs
@@ -1,7 +1,14 @@
 list = Enum.to_list(1..10_000)
 
 Benchee.run [
-  {"map with TCO reverse", fn -> MyMap.map(list, fn(i) -> i + 1 end) end},
-  {"map with TCO and ++", fn -> MyMap.map2(list, fn(i) -> i + 1 end) end},
-  {"map simple without TCO", fn -> MyMap.map3(list, fn(i) -> i + 1 end) end}
+  {"map with TCO reverse",
+   fn -> MyMap.map_tco(list, fn(i) -> i + 1 end) end},
+  {"map with TCO and ++",
+   fn -> MyMap.map_tco_concat(list, fn(i) -> i + 1 end) end},
+  {"map simple without TCO",
+   fn -> MyMap.map_body(list, fn(i) -> i + 1 end) end},
+  {"map TCO no reverse",
+   fn -> MyMap.map_tco_no_reverse(list, fn(i) -> i + 1 end) end},
+  {"stdlib map",
+   fn -> Enum.map(list, fn(i) -> i + 1 end) end}
 ]
--- lib/my_map.ex
+++ lib/my_map.ex
@@ -1,42 +1,50 @@
 defmodule MyMap do
 
   @doc """
-  iex> MyMap.map [1, 2, 3, 4], fn(i) -> i + 1 end
+  iex> MyMap.map_tco [1, 2, 3, 4], fn(i) -> i + 1 end
   [2, 3, 4, 5]
   """
-  def map(list, function) do
-    Enum.reverse _map([], list, function)
+  def map_tco(list, function) do
+    Enum.reverse _map_tco([], list, function)
   end
 
-  defp _map(acc, [head | tail], function) do
-    _map([function.(head) | acc], tail, function)
+  defp _map_tco(acc, [head | tail], function) do
+    _map_tco([function.(head) | acc], tail, function)
   end
 
-  defp _map(acc, [], _function) do
+  defp _map_tco(acc, [], _function) do
     acc
   end
 
   @doc """
-  iex> MyMap.map2 [1, 2, 3, 4], fn(i) -> i + 1 end
+  iex> MyMap.map_tco_concat [1, 2, 3, 4], fn(i) -> i + 1 end
   [2, 3, 4, 5]
   """
-  def map2(acc \\ [], list, function)
+  def map_tco_concat(acc \\ [], list, function)
 
-  def map2(acc, [head | tail], function) do
-    map2(acc ++ [function.(head)], tail, function)
+  def map_tco_concat(acc, [head | tail], function) do
+    map_tco_concat(acc ++ [function.(head)], tail, function)
   end
 
-  def map2(acc, [], _function) do
+  def map_tco_concat(acc, [], _function) do
     acc
   end
 
   @doc """
-  iex> MyMap.map3 [1, 2, 3, 4], fn(i) -> i + 1 end
+  iex> MyMap.map_body [1, 2, 3, 4], fn(i) -> i + 1 end
   [2, 3, 4, 5]
   """
-  def map3([], _func), do: []
+  def map_body([], _func), do: []
 
-  def map3([head | tail], func) do
-    [func.(head) | map(tail, func)]
+  def map_body([head | tail], func) do
+    [func.(head) | map_body(tail, func)]
+  end
+
+  @doc """
+  iex> MyMap.map_tco_no_reverse [1, 2, 3, 4], fn(i) -> i + 1 end
+  [5, 4, 3, 2]
+  """
+  def map_tco_no_reverse(list, function) do
+    _map_tco([], list, function)
   end
 end
