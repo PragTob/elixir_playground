5f850e3f280be2ef03be63df1182ec70468338c8
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-15 18:13:49 +0200

    Update benchee

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule ElixirPlayground.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:benchee, "~> 0.1.0"}]
+    [{:benchee, "~> 0.1"}]
   end
 end
