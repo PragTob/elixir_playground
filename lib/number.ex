a2072e786580e7aef750bb5bc0266219934a4fc8
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-15 18:05:16 +0200

    is_even_tco?

--- lib/number.ex
+++ lib/number.ex
@@ -2,15 +2,37 @@ defmodule Number do
   @doc """
   iex> Number.is_even? 1
   false
+  
   iex> Number.is_even? 2
   true
+
   iex> Number.is_even? 7856
   true
+
   iex> Number.is_even? 8769
   false
   """
   def is_even?(0), do: true
   def is_even?(n) do
     !is_even?(n - 1)
+  end
+
+  @doc """
+  iex> Number.is_even? 1
+  false
+
+  iex> Number.is_even? 2
+  true
+
+  iex> Number.is_even? 7856
+  true
+
+  iex> Number.is_even? 8769
+  false
+  """
+  def is_even_tco?(n, acc \\ true)
+  def is_even_tco?(0, acc), do: acc
+  def is_even_tco?(n, acc) do
+    is_even_tco?(n - 1, acc)
   end
 end
