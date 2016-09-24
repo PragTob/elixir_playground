dab8c10ead38441c0cfd41f8d4a42ade5ae678c2
Tobias Pfeiffer <tobi@bitcrowd.net>
2016-09-24 20:52:12 +0200

    Bigger maps --> more reliable benchmarks

--- bench/deep_merge.exs
+++ bench/deep_merge.exs
@@ -1,10 +1,15 @@
-orig = %{a: %{b: %{c: %{a: 99, d: "foo", e: 2}}, m: 33, i: 99, y: "bar"}, b: 77, y: [23, 87], z: %{ xy: %{y: :x} }}
-new =  %{a: %{b: %{c: %{a: 1, b: 2, d: "bar"}}, m: 12, i: 102}, b: 77, x: 65, y: [23], z: %{ xy: %{x: :y} }}
+base_map = (0..50)
+           |> Enum.zip(300..350)
+           |> Enum.into(%{})
+
+orig = Map.merge base_map, %{150 => %{1 => 1, 2 => 2}, 155 => %{y: :x}, 170 => %{"foo" => "bar"}, z: %{ x: 4, y: %{a: 1, b: %{c: 2}, d: %{"hey" => "ho"}}}, a: %{b: %{c: %{a: 99, d: "foo", e: 2}}, m: [33], i: 99, y: "bar"}, b: %{y: [23, 87]}, z: %{xy: %{y: :x}}}
+new = Map.merge base_map, %{150 => %{3 => 3}, 160 => %{a: "b"}, z: %{ xui: [44], y: %{b: %{c: 77, d: 55}, d: %{"ho" => "hey", "du" => "nu", "hey" => "ha"}}}, a: %{b: %{c: %{a: 1, b: 2, d: "bar"}}, m: 12, i: 102}, b: %{ x: 65, y: [23]}, z: %{ xy: %{x: :y}}}
 
 simple_merge_fun = fn(_key, _base, override) -> override end
 
 Benchee.run %{
   "Map.merge/2"  => fn -> Map.merge orig, new end,
+  "Map.merge/3"  => fn -> Map.merge orig, new, simple_merge_fun end,
   "deep_merge/2" => fn -> DeepMerge.deep_merge orig, new end,
   "deep_merge/3" => fn -> DeepMerge.deep_merge orig, new, simple_merge_fun end
 }
