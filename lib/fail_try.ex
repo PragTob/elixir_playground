28a3122810560ba763ec0e699b772f59095abba4
Tobias Pfeiffer <pragtob@gmail.com>
2016-10-16 15:10:50 +0200

    silence warning

--- lib/fail_try.ex
+++ lib/fail_try.ex
@@ -1,9 +1,13 @@
-defmodule MyFail do
+# Outcommented to not get the warning every time, comment back in to see it:
+# warning: function A.B.nonexistebt/0 is undefined (module A.B is not available)
+# lib/fail_try.ex:6
 
-  alias A.B
-
-  def method do
-    B.nonexistebt
-  end
-
-end
+# defmodule MyFail do
+#
+#   alias A.B
+#
+#   def method do
+#     B.nonexistebt
+#   end
+#
+# end
