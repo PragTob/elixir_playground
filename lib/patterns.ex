f89366d1f41c0de21c6e75209840c686b65c9579
Tobias Pfeiffer <pragtob@gmail.com>
2016-11-01 09:08:10 +0100

    Denis

--- lib/patterns.ex
+++ lib/patterns.ex
@@ -2,12 +2,12 @@ defmodule Patterns do
   def greet(%{name: name, age: age}) do
     IO.puts "Hi there #{name}, what's up at #{age}?"
   end
-
+  def greet(%{name: "Denis Defreyne"}) do
+    IO.puts "Hi Denis, are you all set for your talk?"
+  end
   def greet(%{name: name}) do
     IO.puts "Hi there #{name}"
   end
-
   def greet(_) do
     IO.puts "Hi"
   end
-end
