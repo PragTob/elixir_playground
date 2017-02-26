c0b30c50231b99fd86e3fe521f6ddc29b1271374
Tobias Pfeiffer <pragtob@gmail.com>
2017-02-26 11:38:03 +0100

    Update number benchmark for newer benchee version

--- bench/is_even.exs
+++ bench/is_even.exs
@@ -1,22 +1,33 @@
 number = 10_000_000
 
-Benchee.run %{warmup: 10, time: 10},  [
-  {"is_even?", fn -> Number.is_even?(number) end },
-  {"is_even_tco?", fn -> Number.is_even_tco?(number) end },
-  {"is_even_tco_new_acc?", fn -> Number.is_even_tco_new_acc?(number) end },
-]
+Benchee.run %{
+  "is_even?" => fn -> Number.is_even?(number) end,
+  "is_even_tco?" => fn -> Number.is_even_tco?(number) end,
+  "is_even_tco_new_acc?" => fn -> Number.is_even_tco_new_acc?(number) end,
+}, warmup: 10, time: 10
 
-# tobi@happy ~/github/elixir_playground $ mix run bench/is_even.exs 
+# tobi@speedy ~/github/elixir_playground $ mix run bench/is_even.exs 
+# Compiling 9 files (.ex)
+# Generated elixir_playground app
+# Erlang/OTP 19 [erts-8.1] [source] [64-bit] [smp:8:8] [async-threads:10] [hipe] [kernel-poll:false]
+# Elixir 1.4.0
+# Benchmark suite executing with the following configuration:
+# warmup: 10.0s
+# time: 10.0s
+# parallel: 1
+# inputs: none specified
+# Estimated total run time: 60.0s
+#
 # Benchmarking is_even?...
 # Benchmarking is_even_tco?...
 # Benchmarking is_even_tco_new_acc?...
 #
-# Name                                    ips        average    deviation         median
-# is_even?                              10.05     99528.28μs     (±0.08%)     99510.00μs
-# is_even_tco_new_acc?                   9.39    106468.71μs     (±0.05%)    106458.00μs
-# is_even_tco?                           9.39    106473.06μs     (±0.06%)    106459.00μs
+# Name                           ips        average  deviation         median
+# is_even?                      9.70      103.06 ms    ±17.02%      100.88 ms
+# is_even_tco_new_acc?          8.52      117.43 ms     ±0.21%      117.35 ms
+# is_even_tco?                  8.48      117.91 ms     ±1.32%      117.50 ms
 #
 # Comparison:
-# is_even?                              10.05
-# is_even_tco_new_acc?                   9.39 - 1.07x slower
-# is_even_tco?                           9.39 - 1.07x slower
+# is_even?                      9.70
+# is_even_tco_new_acc?          8.52 - 1.14x slower
+# is_even_tco?                  8.48 - 1.14x slower
