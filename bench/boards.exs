caab47f953259acaffb67113899869de8b167e22
Tobias Pfeiffer <pragtob@gmail.com>
2019-06-13 12:24:02 +0200

    let me introduce you to... mixed bag

--- bench/boards.exs
+++ bench/boards.exs
@@ -41,7 +41,8 @@ Benchee.run(
   },
   pre_check: true,
   time: 0.5,
-  warmup: 0.5
+  warmup: 0.5,
+  print: [benchmarking: false, configuration: false]
 )
 
 IO.puts("""
@@ -60,7 +61,8 @@ Benchee.run(
   },
   pre_check: true,
   time: 0.5,
-  warmup: 0.5
+  warmup: 0.5,
+  print: [benchmarking: false, configuration: false]
 )
 
 IO.puts("""
@@ -79,5 +81,86 @@ Benchee.run(
   },
   pre_check: true,
   time: 0.5,
-  warmup: 0.5
+  warmup: 0.5,
+  print: [benchmarking: false, configuration: false]
+)
+
+IO.puts("""
+
+================= mixed bag ====================
+
+""")
+
+Benchee.run(
+  %{
+    "mixed bag List2D" => fn ->
+      board =
+        board_list_2d
+        |> List2D.set(0, 0, :boing)
+        |> List2D.set(4, 4, :boing)
+        |> List2D.set(8, 8, :boing)
+
+      val1 = List2D.get(board, 0, 0)
+      val2 = List2D.get(board, 4, 4)
+      val3 = List2D.get(board, 8, 8)
+
+      {val1, val2, val3}
+    end,
+    "mixed bag List1D" => fn ->
+      board =
+        board_list_1d
+        |> List1D.set(0, 0, :boing)
+        |> List1D.set(4, 4, :boing)
+        |> List1D.set(8, 8, :boing)
+
+      val1 = List1D.get(board, 0, 0)
+      val2 = List1D.get(board, 4, 4)
+      val3 = List1D.get(board, 8, 8)
+
+      {val1, val2, val3}
+    end,
+    "mixed bag MapTuple" => fn ->
+      board =
+        board_map_tuple
+        |> MapTuple.set(0, 0, :boing)
+        |> MapTuple.set(4, 4, :boing)
+        |> MapTuple.set(8, 8, :boing)
+
+      val1 = MapTuple.get(board, 0, 0)
+      val2 = MapTuple.get(board, 4, 4)
+      val3 = MapTuple.get(board, 8, 8)
+
+      {val1, val2, val3}
+    end,
+    "mixed bag Tuple2D" => fn ->
+      board =
+        board_tuple_2d
+        |> Tuple2D.set(0, 0, :boing)
+        |> Tuple2D.set(4, 4, :boing)
+        |> Tuple2D.set(8, 8, :boing)
+
+      val1 = Tuple2D.get(board, 0, 0)
+      val2 = Tuple2D.get(board, 4, 4)
+      val3 = Tuple2D.get(board, 8, 8)
+
+      {val1, val2, val3}
+    end,
+    "mixed bag Tuple1D" => fn ->
+      board =
+        board_tuple_1d
+        |> Tuple1D.set(0, 0, :boing)
+        |> Tuple1D.set(4, 4, :boing)
+        |> Tuple1D.set(8, 8, :boing)
+
+      val1 = Tuple1D.get(board, 0, 0)
+      val2 = Tuple1D.get(board, 4, 4)
+      val3 = Tuple1D.get(board, 8, 8)
+
+      {val1, val2, val3}
+    end
+  },
+  pre_check: true,
+  time: 4,
+  warmup: 1,
+  print: [benchmarking: false, configuration: false]
 )
