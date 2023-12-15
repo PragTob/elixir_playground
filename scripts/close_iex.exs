f7ee1b8077fa6706242d163af61a9208b69f2b0b
Tobias Pfeiffer <pragtob@gmail.com>
2023-12-15 11:09:01 +0100

    trying to open and close iex gracefully

---
+++ scripts/close_iex.exs
@@ -0,0 +1,22 @@
+# test env to avoid repeated compilation on CI
+port = Port.open({:spawn, "iex"}, [:binary])
+
+# wait for startup
+receive do
+  {^port, {:data, "iex(1)> "}} -> :ok
+end
+
+# attemtps at closing iex in the port
+# send(port, {:command, "^C"})
+# send(port, {:command, "^C"})
+# send(port, {:command, "^\\"})
+# port_info = port |> Port.info() |> IO.inspect()
+# os_pid = Access.get(port_info, :os_pid)
+# System.cmd("kill", [to_string(os_pid)])
+
+# attempts at closing just the port
+Port.close(port)
+# send(port, {self(), :close})
+# receive do
+#   {^port, :closed} -> :ok
+# end
