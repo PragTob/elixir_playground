0e2b214c18352c634b52e2b280e6a3a02b4fa3bf
Tobias Pfeiffer <pragtob@gmail.com>
2023-12-15 10:01:50 +0100

    mix format

--- bench/task_no_task.exs
+++ bench/task_no_task.exs
@@ -6,24 +6,30 @@ end
 inputs = [
   {"10k", random_list.(10_000, 100)},
   {"1M", random_list.(1_000_000, 1_000)},
-  {"10M", random_list.(10_000_000, 10_000)},
+  {"10M", random_list.(10_000_000, 10_000)}
 ]
 
-Benchee.run(%{
-  "sequential" => fn big_list ->
-    uniques = Enum.uniq(big_list)
-    frequencies = Enum.frequencies(big_list)
-    shuffled = Enum.shuffle(big_list)
+Benchee.run(
+  %{
+    "sequential" => fn big_list ->
+      uniques = Enum.uniq(big_list)
+      frequencies = Enum.frequencies(big_list)
+      shuffled = Enum.shuffle(big_list)
 
-    [uniques, frequencies, shuffled]
-  end,
-  "parallel" => fn big_list ->
-    tasks = [
-      Task.async(fn -> Enum.uniq(big_list) end),
-      Task.async(fn -> Enum.frequencies(big_list) end),
-      Task.async(fn -> Enum.shuffle(big_list) end)
-    ]
+      [uniques, frequencies, shuffled]
+    end,
+    "parallel" => fn big_list ->
+      tasks = [
+        Task.async(fn -> Enum.uniq(big_list) end),
+        Task.async(fn -> Enum.frequencies(big_list) end),
+        Task.async(fn -> Enum.shuffle(big_list) end)
+      ]
 
-    Task.await_many(tasks, :infinity)
-  end
-}, inputs: inputs, warmup: 2, time: 40, formatters: [{Benchee.Formatters.Console, extended_statistics: true}])
+      Task.await_many(tasks, :infinity)
+    end
+  },
+  inputs: inputs,
+  warmup: 2,
+  time: 40,
+  formatters: [{Benchee.Formatters.Console, extended_statistics: true}]
+)
