84baa0f149641e3694cd2d0f93be4cbdfca4effc
Tobias Pfeiffer <pragtob@gmail.com>
2023-12-17 18:33:24 +0100

    grab newest benchee_html for new plotly.js

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule ElixirPlayground.Mixfile do
     [
       {:assertions, "~> 0.10", only: :test},
       {:benchee, "~> 1.0", github: "bencheeorg/benchee", override: true},
-      {:benchee_html, "~> 1.0"},
+      {:benchee_html, "~> 1.0", github: "bencheeorg/benchee_html"},
       {:credo, "~> 0.4"},
       {:ex_guard, "~> 1.3", only: :dev},
       {:mox, "~> 0.5.1"},
